93351d07ff7c527f8d02db0ea516b5f3587ad7cb
Jhainey <jhainey.perz@dropi.co>
2024-06-28 18:08:38 -0500

    add Input component

---
+++ app/components/shared/Input.tsx
@@ -0,0 +1,9 @@
+export default function Input(props) {
+  const { ...rest } = props;
+  return (
+    <input
+      {...rest}
+      className="rounded-sm bg-stone-800 outline-0 text-white py-2 px-4 my-2"
+    ></input>
+  );
+}
--- app/routes/add-category-form/_index.tsx
+++ app/routes/add-category-form/_index.tsx
@@ -1,11 +1,12 @@
 import { Form } from "@remix-run/react";
 import { ButtonPrimary } from "~/components/shared/ButtonPrimary";
+import Input from "~/components/shared/Input";
 
 export default function AddCategoryForm() {
   return (
     <Form method="POST" action="" className="flex flex-col">
-      <input name="title" placeholder="Titulo" className="my-2"></input>
-      <input name="slug" placeholder="Slug"></input>
+      <Input name="title" placeholder="Titulo" className="my-2"></Input>
+      <Input name="slug" placeholder="Slug"></Input>
       <ButtonPrimary type="submit" className="mt-3">
         Crear categoria
       </ButtonPrimary>

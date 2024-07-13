5c8d9eb74ea2fac1005ecb3b055e67eb8575a1ca
Jhainey <jhainey.perz@dropi.co>
2024-07-13 17:02:31 -0500

    start post creation form

--- app/components/Menu.tsx
+++ app/components/Menu.tsx
@@ -1,10 +1,12 @@
-import { Link } from "@remix-run/react";
+import { Link, useParams } from "@remix-run/react";
 import { ButtonPrimary } from "./shared/ButtonPrimary";
 import Modal from "./shared/Modal";
 import { useState } from "react";
 import AddCategoryForm from "~/routes/add-category-form/route";
+import AddPost from "~/routes/add-post-form/route";
 
 export function Menu() {
+  const { category } = useParams();
   const [show, setShow] = useState(false);
   const [content, setContent] = useState(null);
 
@@ -12,7 +14,7 @@ export function Menu() {
   const addPost = () => {
     console.log("crear post");
     onOpenCloseModal();
-    setContent(null);
+    setContent(<AddPost />);
   };
   const addCategoria = () => {
     console.log("crear categoria");
@@ -22,15 +24,21 @@ export function Menu() {
   return (
     <>
       <div className="bg-stone-800 flex items-center justify-between py-50 px-10">
+        <div />
         <Link to="/">
           <h1 className="text-white text-2xl"> DevBlog </h1>
         </Link>
 
         <div>
-          <ButtonPrimary className="bg-red-500" onClick={addPost}>
-            Crear post
-          </ButtonPrimary>
-          <ButtonPrimary onClick={addCategoria}>Crear categoria</ButtonPrimary>
+          {category ? (
+            <ButtonPrimary className="bg-red-500" onClick={addPost}>
+              Crear post
+            </ButtonPrimary>
+          ) : (
+            <ButtonPrimary onClick={addCategoria}>
+              Crear categoria
+            </ButtonPrimary>
+          )}
         </div>
       </div>
 
---
+++ app/routes/add-post-form/route.tsx
@@ -0,0 +1,44 @@
+import { Form, redirect, useParams } from "@remix-run/react";
+import { ButtonPrimary } from "~/components/shared/ButtonPrimary";
+import Input from "~/components/shared/Input";
+import TextArea from "~/components/shared/TextArea";
+
+export default function AddPost() {
+  const { category } = useParams();
+  return (
+    <Form method="POST" action="/add-post-form" className="flex flex-col">
+      <Input name="title" placeholder="Titulo" className="my-2"></Input>
+      <Input
+        name="description"
+        placeholder="Description"
+        className="my-2"
+      ></Input>
+      <Input name="slug" placeholder="Slug" className="my-2"></Input>
+      <Input name="category" value={category} className="my-2"></Input>
+      <TextArea
+        name="content"
+        placeholder="Contenido"
+        className="my-2"
+        rows="6"
+      />
+      <ButtonPrimary type="submit" className="mt-3">
+        Crear post
+      </ButtonPrimary>
+    </Form>
+  );
+}
+
+export async function action(props) {
+  const { request } = props;
+  const formData = await request.formData();
+
+  const category = formData.get("category");
+  const data = {
+    title: formData.get("title"),
+    slug: formData.get("slug"),
+    description: formData.get("description"),
+    content: formData.get("content"),
+  };
+
+  return redirect("/");
+}
